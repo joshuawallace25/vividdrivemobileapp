4ac1e4da06222a3f090edfd142ebfea07fa6a7ef
joshuawallace25 <131925695+joshuawallace25@users.noreply.github.com>
2025-03-15 13:22:34 +0200

    android bug fixed

--- lib/Component/buttomNavbar.dart
+++ lib/Component/buttomNavbar.dart
@@ -1,9 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:vividdrive/screens/AlbumScreen/albumscreen.dart';
 import 'package:vividdrive/screens/testscreen/testscreen.dart';
-
 
 // ignore: must_be_immutable
 class Navbar extends StatefulWidget {
@@ -17,29 +15,27 @@ class Navbar extends StatefulWidget {
 
 class _NavbarState extends State<Navbar> {
   List Screens = [AlbumScreen(), TestScreen()];
-    int _selectedIndex = 0;
+  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-        backgroundColor: Colors.blue[800],
-        bottomNavigationBar: CurvedNavigationBar(
-          index: _selectedIndex,
-          color: Colors.blue,
-          backgroundColor: Colors.white,
-          items: [
-            const Icon(Icons.home_filled, color: Colors.white),
-             const Icon(Icons.album_outlined, color: Colors.white),
-          ],
-           animationDuration: const Duration(milliseconds: 300), 
-          onTap: (index) {
-            setState(() {
-             _selectedIndex  = index;
-            });
-          },
-          
-        ),
-        body: Screens[_selectedIndex],
+      backgroundColor: Colors.blue[800],
+      bottomNavigationBar: CurvedNavigationBar(
+        index: _selectedIndex,
+        color: Colors.blue,
+        backgroundColor: Colors.white,
+        items: [
+          const Icon(Icons.home_filled, color: Colors.white),
+          const Icon(Icons.album_outlined, color: Colors.white),
+        ],
+        animationDuration: const Duration(milliseconds: 300),
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
+      body: Screens[_selectedIndex],
     );
   }
 }
